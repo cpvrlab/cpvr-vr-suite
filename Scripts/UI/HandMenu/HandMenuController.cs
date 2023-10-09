be6c239e7f3c1b77391bb34ab5041db802aa2fc1
Sascha Ledermann <saschaledermann@gmail.com>
2023-10-09 17:04:14 +0200

    Started creating Rig v3

--- Scripts/UI/HandMenu/HandMenuController.cs
+++ Scripts/UI/HandMenu/HandMenuController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
@@ -11,13 +12,17 @@ public class HandMenuController : MonoBehaviour
     [SerializeField] private SoundClip _hoverClip;
     [SerializeField] private SoundClip _clickClip;
 
+    [Header("Events")]
+    [SerializeField] private UnityEvent _onEnable;
+    [SerializeField] private UnityEvent _onDisable;
+
     [Header("UI Panels")] 
     [SerializeField] private List<MenuPanel> panels;
     private AudioSource _audioSource;
 
     private void Start()
     {
-        if (!TryGetComponent<AudioSource>(out _audioSource))
+        if (!TryGetComponent(out _audioSource))
         {
             Debug.LogWarning("No AudioSource attached. Adding one.");
             _audioSource = gameObject.AddComponent<AudioSource>();
@@ -28,7 +33,13 @@ public class HandMenuController : MonoBehaviour
 
     private void OnEnable()
     {
+        _onEnable?.Invoke();
         OpenMainPanel();
+    }
+
+    private void OnDisable()
+    {
+        _onDisable?.Invoke();
     }
 
     public void OpenPanel(int index)
