cf370c061b8e370e44c889fc20c19be2fa2812dc
Marcus Hudritsch <marcus.hudritsch@bfh.ch>
2023-11-02 08:19:19 +0100

    Fixed ScaleInOnStartBehaviour

--- Scripts/Util/ScaleInOnStartBehaviour.cs
+++ Scripts/Util/ScaleInOnStartBehaviour.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace cpvr_vr_suite.Scripts.Util
 {
@@ -9,6 +10,8 @@ namespace cpvr_vr_suite.Scripts.Util
         public GameObject objectToScaleIn;
         public List<GameObject> objectsToShowAfterFadeIn;
         public float transitionTime = 3.0f;
+        
+        private InitializeTeleportationAreas _initTeleportAreaScript; 
 
         private void Awake()
         {
@@ -16,6 +19,8 @@ namespace cpvr_vr_suite.Scripts.Util
 
             foreach (var objToActivate in objectsToShowAfterFadeIn)
                 objToActivate.SetActive(false);
+
+            _initTeleportAreaScript = FindObjectOfType<InitializeTeleportationAreas>();
         }
 
         // Start is called before the first frame update
@@ -38,6 +43,8 @@ namespace cpvr_vr_suite.Scripts.Util
 
             foreach (var objToActivate in objectsToShowAfterFadeIn)
                 objToActivate.SetActive(true);
+
+            _initTeleportAreaScript.CreateTeleportAreas(SceneManager.GetActiveScene());
         }
     }
 }
