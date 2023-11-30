a761a8d6c9b29e7b8739f4a42948b4999f967e92
Sascha Ledermann <saschaledermann@gmail.com>
2023-11-30 13:29:47 +0100

    Added Analyzer which generates a report of useful metrics

--- Scripts/Util/MailSender.cs
+++ Scripts/Util/MailSender.cs
@@ -55,5 +55,48 @@ namespace cpvrlab_vr_suite.Scripts.Util
                 return false;
             }
         }
+
+        public static async Task<bool> SendEmail(string receiver, string subject, string message, string attachmentPath)
+        {
+            try
+            {
+                var mailData = LoadJsonData.Load("Secrets/EmailLogin");
+                if (mailData == null)
+                {
+                    Debug.LogError("No valid MailData object returned!");
+                    return false;
+                }
+
+                SmtpClient smtpClient = new SmtpClient(mailData.server, mailData.port);
+                smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.EnableSsl = true;
+
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress(mailData.address);
+                mail.To.Add(new MailAddress(receiver));
+                mail.Subject = subject;
+                mail.Body = message;
+            
+                var attachment = new Attachment(attachmentPath, MediaTypeNames.Application.Octet);
+                var disposition = attachment.ContentDisposition;
+                disposition.CreationDate = File.GetCreationTime(attachmentPath);
+                disposition.ModificationDate = File.GetLastWriteTime(attachmentPath);
+                disposition.ReadDate = File.GetLastAccessTime(attachmentPath);
+                mail.Attachments.Add(attachment);
+
+                smtpClient.Timeout = 5000;
+                smtpClient.EnableSsl = true;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new NetworkCredential(mailData.address, mailData.apiKey);
+                await smtpClient.SendMailAsync(mail);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e);
+                return false;
+            }
+        }
     }
 }
