95d2b61bc2b02894cb22239bc46a79d9d7f78c2b
Sascha Ledermann <saschaledermann@gmail.com>
2024-01-15 14:52:47 +0100

    Added delegate for OnEnable()

--- Scripts/UI/HandMenu/SceneSelectionPanel.cs
+++ Scripts/UI/HandMenu/SceneSelectionPanel.cs
@@ -7,17 +7,19 @@ using UnityEngine.UI;
 
 public class SceneSelectionPanel : MenuPanel
 {
+    public delegate void OnEnableHandler();
+    public OnEnableHandler onEnableHandler;
     [SerializeField] private GameObject buttonPrefab;
     [SerializeField] private Transform scrollviewContent;
-    private readonly List<Button> _sceneButtons = new();
+    private readonly List<Button> m_sceneButtons = new();
 
-    public IEnumerable<Button> SceneButtons {get => _sceneButtons.AsReadOnly();}
+    public IEnumerable<Button> SceneButtons {get => m_sceneButtons.AsReadOnly();}
 
     protected override void Start()
     {
         base.Start();
         
-        _sceneButtons.Add(default); // Dummy object to occupy the first index in the list so the button indexes match with the scene indexes
+        m_sceneButtons.Add(default); // Dummy object to occupy the first index in the list so the button indexes match with the scene indexes
         for (var i = 1; i < SceneManager.sceneCountInBuildSettings; i++)
         {
             var label = System.IO.Path.GetFileNameWithoutExtension(SceneUtility.GetScenePathByBuildIndex(i)).ToString();
@@ -32,16 +34,18 @@ public class SceneSelectionPanel : MenuPanel
         buttonObject.GetComponentInChildren<TextMeshProUGUI>().text = label;
         var button = buttonObject.GetComponent<Button>();
         button.onClick.AddListener(() => callback.Invoke(argument));
-        _sceneButtons.Add(button);
+        m_sceneButtons.Add(button);
         _handMenuController.AddButtonSoundFeedback(button);
         return button;
     }
 
-    private void ChangeScene(int index)
+    void ChangeScene(int index)
     {
         var currentIndex = SceneManager.GetActiveScene().buildIndex;
         if (index == currentIndex) return;
         _handMenuController.UnregisterDynamicPanels();
         SceneManager.LoadSceneAsync(index);
     }
+
+    void OnEnable() => onEnableHandler?.Invoke();
 }
