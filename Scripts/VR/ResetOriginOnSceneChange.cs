b432cfa2b6199a16b739ca0191a8a3d78100e925
Sascha Ledermann <saschaledermann@gmail.com>
2024-02-01 15:41:51 +0100

    The ResetPosition can now be set from the outside

--- Scripts/VR/ResetOriginOnSceneChange.cs
+++ Scripts/VR/ResetOriginOnSceneChange.cs
@@ -5,7 +5,16 @@ namespace cpvr_vr_suite.Scripts.VR
 {
     public class ResetOriginOnSceneChange : MonoBehaviour
     {
-        private void OnEnable() =>
-            SceneManager.activeSceneChanged += (_, _) => transform.position = Vector3.zero;
+        public Vector3 ResetPosition { get; set; }
+
+        private void OnEnable()
+        {
+            ResetPosition = Vector3.zero;
+            SceneManager.activeSceneChanged += (_, _) =>
+            {
+                transform.position = ResetPosition;
+                ResetPosition = Vector3.zero;
+            };
+        }
     }
 }
