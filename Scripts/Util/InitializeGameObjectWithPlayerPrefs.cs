8db80038a770d10909909f7e56d1ad3b40f982cb
Sascha Ledermann <sascha.ledermann@bfh.ch>
2024-02-13 09:24:53 +0100

    Gaze can now be set as active by default

--- Scripts/Util/InitializeGameObjectWithPlayerPrefs.cs
+++ Scripts/Util/InitializeGameObjectWithPlayerPrefs.cs
@@ -2,11 +2,19 @@ using UnityEngine;
 
 public class InitializeGameObjectWithPlayerPrefs : MonoBehaviour
 {
-    [SerializeField] private string _playerprefsValue;
+    [SerializeField] string m_playerprefsValue;
+    [SerializeField] bool m_defaultValue;
 
     void Start()
     {
-        int value = PlayerPrefs.GetInt(_playerprefsValue);
-        gameObject.SetActive(value == 1);
+        if (PlayerPrefs.HasKey(m_playerprefsValue))
+        {
+            int value = PlayerPrefs.GetInt(m_playerprefsValue);
+            gameObject.SetActive(value == 1);
+        }
+        else
+        {
+            gameObject.SetActive(m_defaultValue);
+        }
     }
 }
