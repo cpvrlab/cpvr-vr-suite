b4445bd97956da6a37c0f08d3d541670a1933f5f
Sascha Ledermann <saschaledermann@gmail.com>
2024-03-20 15:33:49 +0100

    Fixed faulty unity ray interaction

--- Scripts/VR/RayStabilizer.cs
+++ Scripts/VR/RayStabilizer.cs
@@ -1,16 +1,10 @@
 using System;
 using UnityEngine;
-
-public enum Handedness
-{
-    None,
-    Left,
-    Right
-}
+using UnityEngine.XR.Hands;
 
 public class RayStabilizer : MonoBehaviour
 {
-    [SerializeField] Transform m_target;
+    [SerializeField] XRHandTrackingEvents m_XRHandTrackingEvents;
     [SerializeField] Handedness m_handedness;
     [SerializeField][Range(-2f, 1f)] float m_yOffset;
     Transform m_headTransform;
@@ -26,9 +20,6 @@ public class RayStabilizer : MonoBehaviour
 
     void Update()
     {
-        if (m_target != null)
-            transform.position = m_target.position;
-
         var offset = GetHeadOffset();
         var direction = GetDirection(offset);
         transform.rotation = Quaternion.LookRotation(direction);
@@ -36,8 +27,14 @@ public class RayStabilizer : MonoBehaviour
 
     void OnEnable()
     {
-        if (m_target != null)
-            transform.position = m_target.position;
+        if (m_XRHandTrackingEvents != null)
+                m_XRHandTrackingEvents.jointsUpdated.AddListener(OnJointsUpdated);
+    }
+
+    void OnDisable()
+    {
+        if (m_XRHandTrackingEvents != null)
+                m_XRHandTrackingEvents.jointsUpdated.RemoveListener(OnJointsUpdated);
     }
 
     Vector3 GetHeadOffset()
@@ -66,10 +63,31 @@ public class RayStabilizer : MonoBehaviour
 
     Vector3 GetDirection(Vector3 offset) => (transform.position - offset).normalized;
 
+    void OnJointsUpdated(XRHandJointsUpdatedEventArgs args)
+    {
+        var thumbTip = args.hand.GetJoint(XRHandJointID.ThumbTip);
+        if (!thumbTip.TryGetPose(out var thumbTipPose))
+            return;
+
+        var indexTip = args.hand.GetJoint(XRHandJointID.IndexTip);
+        if (!indexTip.TryGetPose(out var indexTipPose))
+            return;
+
+        var targetPos = Vector3.Lerp(thumbTipPose.position, indexTipPose.position, 0.5f);
+        transform.localPosition = targetPos;
+    }
+
     void OnDrawGizmos()
     {
         if (!Application.isPlaying) return;
         Gizmos.color = Color.red;
         Gizmos.DrawSphere(GetHeadOffset(), 0.01f);
+    }
+
+    public enum Handedness
+    {
+        None,
+        Left,
+        Right
     }
 }
