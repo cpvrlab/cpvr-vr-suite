1a0dea5f8b5b435d64533e8638c9536a9ff85195
Sascha Ledermann <saschaledermann@gmail.com>
2024-06-20 13:41:26 +0200

    Added multiplayer tools package

--- Scripts/UI/HandMenu/NetworkPanel.cs
+++ Scripts/UI/HandMenu/NetworkPanel.cs
@@ -1,18 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class NetworkPanel : MonoBehaviour
+public class NetworkPanel : MenuPanel
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject m_mainContent;
+    [SerializeField] GameObject m_lobbyContent;
+    [SerializeField] TMP_Text m_title;
+    [SerializeField] Button m_hostButton;
+    [SerializeField] Toggle m_lanToggle;
+    [SerializeField] TMP_InputField m_joincodeInputField;
+    [SerializeField] Button m_clientButton;
+    [SerializeField] TMP_Text m_infoText;
+
+    void Awake()
     {
-        
+        // Setup UI Elements
+        m_hostButton.onClick.AddListener(StartHost);
+
+        m_lanToggle.isOn = true;
+        m_lanToggle.interactable = false;
+
+        m_clientButton.onClick.AddListener(StartClient);
+
+        UpdateInfoText(string.Empty);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnEnable()
     {
-        
+        // Check whether the player is already in a lobby or not
+        m_mainContent.SetActive(true);
+        m_lobbyContent.SetActive(false);
+        m_title.text = "Multiplayer";
+        UpdateInfoText(string.Empty);
     }
+
+    void StartHost()
+    {
+        UpdateInfoText("Starthost clicked!");
+    }
+
+    void StartClient()
+    {
+        UpdateInfoText("StartClient clicked!");
+    }
+
+    void UpdateInfoText(string content) => m_infoText.text = content;
 }
