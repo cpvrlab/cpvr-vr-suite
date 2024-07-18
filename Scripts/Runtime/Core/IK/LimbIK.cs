dc54570a64101ef2090b5dadeb5928436cd11090
Sascha Ledermann <saschaledermann@gmail.com>
2024-07-18 17:10:06 +0200

    Fixed LimbIK Nullreference bug

--- Scripts/Runtime/Core/IK/LimbIK.cs
+++ Scripts/Runtime/Core/IK/LimbIK.cs
@@ -1,33 +1,37 @@
 using System;
 using UnityEngine;
 
-namespace Util {
+namespace Util
+{
     /// <summary>
     /// An IK solution for two limbs / one joint.
     /// </summary>
-    public class LimbIK : MonoBehaviour {
+    public class LimbIK : MonoBehaviour
+    {
         // Where the hand should be placed
         public Transform target;
 
         // Position helper for the flexion of the elbow
         public Transform pole;
 
-        Transform _elbow;
-        Transform _shoulder;
-                    
+        Transform m_elbow;
+        Transform m_shoulder;
+
         // Length of the limbs and the total.
         float _shoulderToElbowLength, _elbowToHandLength, _limbsLength;
 
-        void Start() {
-            _elbow = transform.parent;
-            _shoulder = _elbow.parent;
+        void Start()
+        {
+            m_elbow = transform.parent;
+            m_shoulder = m_elbow.parent;
 
             ComputeLength();
         }
 
-        void FixedUpdate() {
+        void FixedUpdate()
+        {
             // Positions of the joints and extremities.
-            Vector3 shoulderPosition = _shoulder.position;
+            Vector3 shoulderPosition = m_shoulder.position;
             Vector3 targetPosition = target.position;
 
             float shoulderToTargetLength = Vector3.Distance(shoulderPosition, targetPosition);
@@ -39,25 +43,28 @@ namespace Util {
             // Normal used to make the elbow point towards the pole (stay on the target/shoulder/pole's plane).
             Vector3 armNormal = Vector3.Cross(shoulderToPole, shoulderToTarget);
 
-            _shoulder.LookAt(targetPosition, armNormal);
-            _shoulder.localRotation *= Quaternion.Euler(90, 0, 0);
-            _shoulder.localRotation *= Quaternion.Euler(0, 90, 0);
+            m_shoulder.LookAt(targetPosition, armNormal);
+            m_shoulder.localRotation *= Quaternion.Euler(90, 0, 0);
+            m_shoulder.localRotation *= Quaternion.Euler(0, 90, 0);
 
-            if (shoulderToTargetLength > _limbsLength) {
+            if (shoulderToTargetLength > _limbsLength)
+            {
                 // If the target is too far for our limbs we just reach as far as possible
-                _elbow.localRotation = Quaternion.Euler(0, 0, 0);
-            } else {
+                m_elbow.localRotation = Quaternion.Euler(0, 0, 0);
+            }
+            else
+            {
                 // Cosine law to calculate the shoulder and the elbow relative up angle.
                 float shoulderAngle = -CosineAngle(_shoulderToElbowLength, shoulderToTargetLength, _elbowToHandLength);
                 float elbowAngle = -CosineAngle(_shoulderToElbowLength, _elbowToHandLength, shoulderToTargetLength);
 
-                _shoulder.localRotation *= Quaternion.Euler(shoulderAngle, 0, 0);
-                _elbow.localRotation = Quaternion.Euler(elbowAngle+180, 0, 0);
+                m_shoulder.localRotation *= Quaternion.Euler(shoulderAngle, 0, 0);
+                m_elbow.localRotation = Quaternion.Euler(elbowAngle + 180, 0, 0);
             }
 
             transform.rotation = target.rotation;
         }
-        
+
         /*
          * 
          *                     /|  
@@ -71,29 +78,34 @@ namespace Util {
          *  Calculate angle from cosine law
          * 
          */
-        float CosineAngle(float adj1, float adj2, float opp) {
-            return (float)Math.Acos((adj1*adj1 + adj2*adj2 - opp*opp) /(2 * adj1 * adj2)) * Mathf.Rad2Deg;
+        float CosineAngle(float adj1, float adj2, float opp)
+        {
+            return (float)Math.Acos((adj1 * adj1 + adj2 * adj2 - opp * opp) / (2 * adj1 * adj2)) * Mathf.Rad2Deg;
         }
 
         /// <summary>
         /// Compute the length between shoulder/elbow, elbow/wrist and total.
         /// </summary>
-        public void ComputeLength() {
-            Vector3 elbowPosition = _elbow.position;
-            Vector3 shoulderPosition = _shoulder.position;
-            
+        public void ComputeLength()
+        {
+            if (m_elbow == null || m_shoulder == null) return;
+
+            Vector3 elbowPosition = m_elbow.position;
+            Vector3 shoulderPosition = m_shoulder.position;
+
             _shoulderToElbowLength = Vector3.Distance(shoulderPosition, elbowPosition);
             _elbowToHandLength = Vector3.Distance(elbowPosition, transform.position);
             _limbsLength = _shoulderToElbowLength + _elbowToHandLength;
         }
 
-        void OnDrawGizmos() {
-            if(!Application.isPlaying) return;
+        void OnDrawGizmos()
+        {
+            if (!Application.isPlaying) return;
 
             Vector3 polePosition = pole.position;
-            Vector3 shoulderPosition = _shoulder.position;
+            Vector3 shoulderPosition = m_shoulder.position;
             Vector3 targetPosition = target.position;
-        
+
             Gizmos.color = Color.green;
             Gizmos.DrawSphere(polePosition, .01f);
             Gizmos.color = Color.blue;
@@ -102,7 +114,7 @@ namespace Util {
             Gizmos.color = Color.red;
             Gizmos.DrawLine(targetPosition, shoulderPosition);
             Gizmos.color = Color.green;
-            Gizmos.DrawLine((targetPosition + shoulderPosition)/2, polePosition);
+            Gizmos.DrawLine((targetPosition + shoulderPosition) / 2, polePosition);
         }
     }
 }
