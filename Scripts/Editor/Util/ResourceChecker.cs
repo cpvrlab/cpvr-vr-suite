b83f3c1b6a10f8f4fc8ab42a8705f0eabd21e27e
Sascha Ledermann <saschaledermann@gmail.com>
2024-11-07 16:37:04 +0100

    Removed deprecated code

--- Scripts/Editor/Util/ResourceChecker.cs
+++ Scripts/Editor/Util/ResourceChecker.cs
@@ -974,7 +974,7 @@ public class ResourceChecker : EditorWindow {
 			return (T[])meshfilters.ToArray ();
 		}
 		else
-			return (T[])FindObjectsOfType(typeof(T));
+			return (T[])FindObjectsByType<T>(FindObjectsSortMode.None);
 	}
 
 	private TextureDetails GetTextureDetail(Texture tTexture, Material tMaterial, MaterialDetails tMaterialDetails)
